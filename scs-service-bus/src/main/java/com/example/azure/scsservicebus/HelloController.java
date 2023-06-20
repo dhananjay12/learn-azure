c0a322e3a9aa25699b12d8a99261bb3ab0383997
Dhananjay Singh <dhananjay@backbase.com>
2023-06-20 23:05:28 +0200

    test

--- scs-service-bus/src/main/java/com/example/azure/scsservicebus/HelloController.java
+++ scs-service-bus/src/main/java/com/example/azure/scsservicebus/HelloController.java
@@ -1,43 +1,50 @@
 package com.example.azure.scsservicebus;
 
-import com.azure.spring.messaging.checkpoint.Checkpointer;
-import java.util.function.Consumer;
 import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.JSONObject;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import static com.azure.spring.messaging.AzureHeaders.CHECKPOINTER;
+import java.util.function.Consumer;
 
 @RestController
 @Slf4j
 public class HelloController {
 
-    private StreamBridge streamBridge;
+    private final StreamBridge streamBridge;
 
     public HelloController(StreamBridge streamBridge) {
         this.streamBridge = streamBridge;
     }
 
-    @PostMapping("/hello" )
+    @PostMapping("/hello")
     public void register(@RequestParam(defaultValue = "test") String param) {
-        log.info("/hello called with param=>"+param);
+        log.info("/hello called with param=>" + param);
         streamBridge.send("hello-out-0", param);
+    }
+
+    @GetMapping("/test")
+    public void test() {
+        JSONObject jsonObject = new JSONObject();
+
+        for (int i = 0; i <= 3000; i++) {
+            String key = "description" + i;
+            String value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque cursus, nisl sed vestibulum tempus, " +
+                    "lectus leo venenatis massa, ut ullamcorper libero enim sit amet mi. Fusce aliquam aliquet augue, " +
+                    "vitae pellentesque ex dignissim quis. Sed et neque ligula. Vivamus elementum varius lobortis. Sed " +
+                    "efficitur, lorem vel efficitur faucibus, libero ex iaculis nisi, id consectetur massa orci ac tortor. " +
+                    "Nullam malesuada lectus enim, vitae bibendum ex pulvinar nec. Nulla facilisi. Suspendisse potenti.";
+            jsonObject.put(key, value);
+        }
+
+        streamBridge.send("hello-out-0", jsonObject);
     }
 
     @Bean
     public Consumer<Message<String>> consume() {
-        return message -> {
-            Checkpointer checkpointer = (Checkpointer) message.getHeaders().get(CHECKPOINTER);
-            log.info("New message received: '{}'", message.getPayload());
-            checkpointer.success()
-                    .doOnSuccess(s -> log.info("Message '{}' successfully checkpointed", message.getPayload()))
-                    .doOnError(e -> log.error("Error found", e))
-                    .block();
-        };
+        return message -> log.info("New message received: '{}'", message.getPayload());
     }
 
 }
